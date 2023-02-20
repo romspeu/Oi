2bcb3de884e59a1f1f78fc16342a8642ca4a5ae8
romspeu <114173207+romspeu@users.noreply.github.com>
2023-02-21 00:22:37 +0200

    add command-line argument option
    ./start-spark-master.sh -i <IP_ADDRESS_OF_FIRST_SERVER>

--- master-setup.sh
+++ master-setup.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Set the IP address of the first server (the Spark master)
-FIRST_SERVER_IP=<IP_ADDRESS_OF_FIRST_SERVER>
+while getopts "i:" arg; do
+  case $arg in
+    i)
+      FIRST_SERVER_IP=${OPTARG}
+      ;;
+    *)
+      echo "Error: Invalid argument provided"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "${FIRST_SERVER_IP}" ]
+then
+  echo "Error: No IP address provided for the Spark master"
+  echo "Usage: $0 -i <IP_ADDRESS_OF_FIRST_SERVER>"
+  exit 1
+fi
 
 # Start the Spark master
 cd /path/to/spark-3.3.2-bin-hadoop3
